2cf0cd6400339b09cd1da21aaeefaff65b2b0533
SandeshGC <gcsandesh01@gmail.com>
2023-01-08 16:09:28 +0545

    generate random numbers, add roll button

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
+import React from "react";
 import "./App.css";
 import Die from "./components/Die";
 
 function App() {
-  const dieNumArr = [];
-  for (let i = 0; i < 6; i++) {
-    dieNumArr.push(Math.floor(Math.random() * 6));
-  }
-  console.log(dieNumArr)
-  return (
-    <main className="App w-full h-screen flex justify-center items-center bg-slate-800">
-      <div className="innerBox bg-slate-200 h-1/2 p-3 flex flex-col justify-center items-center rounded-lg">
-        <div className="grid gap-6 grid-cols-5">
-          <Die value={1} />
-          <Die value={2} />
-          <Die value={3} />
-          <Die value={4} />
-          <Die value={5} />
-          {/* end of first col */}
-          <Die value={6} />
-          <Die value={4} />
-          <Die value={1} />
-          <Die value={2} />
-          <Die value={4} />
-        </div>
-      </div>
-    </main>
-  );
+	const [dice, setDice] = React.useState(getNewDice());
+	function getNewDice() {
+		const dieNumArr = [];
+		for (let i = 0; i < 10; i++) {
+			dieNumArr.push({ value: Math.ceil(Math.random() * 6), isHeld: false }); //random from 0 to 5, ceiled so that 1 to 6
+		}
+		// console.log(dieNumArr);
+		return dieNumArr;
+	}
+	const diceArr = dice.map((die) => {
+		return <Die value={die.value} />;
+	});
+	console.log(dice);
+	return (
+		// app
+		<main className="App w-full h-screen flex justify-center items-center bg-[#0b2434]">
+			{/* inside white box */}
+			<div className="bg-[#f5f5f5] h-1/2 max-h-96 p-6 flex flex-col justify-center items-center gap-4 rounded-lg">
+				{/* numbers container */}
+				<div className="grid gap-6 grid-cols-5">{diceArr}</div>
+				<button
+					onClick={() => setDice(getNewDice())}
+					className="bg-[#5035ff] rounded-md px-4 py-2 text-white font-semibold shadow-md active:shadow-black active:shadow-inner "
+				>
+					Roll
+				</button>
+			</div>
+		</main>
+	);
 }
 
 export default App;
--- src/components/Die.js
+++ src/components/Die.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Die({ value }) {
   return (
-    <div className="bg-white cursor-pointer font-semibold border border-gray-500 shadow-md rounded-md shadow-gray-400 sm:w-12 sm:h-12 w-8 h-8 flex justify-center items-center">
-      {value}
+    <div className="bg-white cursor-pointer shadow-sm hover:shadow-lg active:shadow-none active:bg-green-400 rounded-md shadow-gray-400 sm:w-12 sm:h-12 w-8 h-8 flex justify-center items-center">
+      <p className=" font-semibold">{value}</p>
     </div>
   );
 }
